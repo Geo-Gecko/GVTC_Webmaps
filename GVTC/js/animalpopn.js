0e94724745f32b69e8aa2d0609cfde12f64b10d8
Geo-Gecko <bernard@geogecko.com>
2019-09-25 15:37:52 +0300

    initial commit

---
+++ GVTC/js/animalpopn.js
@@ -0,0 +1,216 @@
+// Trend graph
+var ctx = document.getElementsByClassName("line-chart");
+
+//Type, data, options
+
+var chartGraph = new Chart (ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            borderWidth: 2,
+            borderColor: 'rgba(146, 242, 42, 0.85)',
+            fill: true,
+            backgroundColor: 'rgba(26, 255, 102, 0.4)'
+        }, {
+            borderWidth: 2,
+            borderColor: 'rgba(207, 0, 15, 0.85)',
+            fill: true,
+            backgroundColor: 'rgba(255, 51, 51, 0.4)'
+        },{
+            borderWidth: 2,
+            borderColor: 'rgba(255, 102, 0, 1)',
+            fill: true,
+            backgroundColor: 'rgba(255, 163, 102, 0.9)'
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 14000,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'keyspecies.xlsx'
+            }
+        }
+    }
+});
+
+
+// Virunga: Hippos graph
+var ctx = document.getElementsByClassName("bar-chart1");
+
+//Type, data, options
+
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(255, 165, 0, 1)',
+            fill: false,
+            backgroundColor: 'rgba(255, 165, 0, 0.6)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 12000,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'virungaHippos.xlsx'
+            }
+        }
+    }
+});
+
+
+//virunga: gorillas graph
+var ctx = document.getElementsByClassName("bar-chart2");
+
+//Type, data, options
+
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(255, 165, 0, 1)',
+            fill: false,
+            backgroundColor: 'rgba(255, 165, 0, 0.6)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 12000,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'virungaGorillas.xlsx'
+            }
+        }
+    }
+});
+
+
+// Bwindi: Hippos graph
+var ctx = document.getElementsByClassName("bar-chart3");
+
+//Type, data, options
+
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(255, 26, 26, 1)',
+            fill: false,
+            backgroundColor: 'rgba(255, 51, 51, 0.5)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 12000,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'queenHippos.xlsx'
+            }
+        }
+    }
+});
+
+
+// Bwindi: gorillas graph
+var ctx = document.getElementsByClassName("bar-chart4");
+
+//Type, data, options
+
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(255, 26, 26, 1)',
+            fill: false,
+            backgroundColor: 'rgba(255, 51, 51, 0.5)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 12000,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'queenGorillas.xlsx'
+            }
+        }
+    }
+});
+
+// Bwindi: Elephants graph
+var ctx = document.getElementsByClassName("bar-chart5");
+
+//Type, data, options
+
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(255, 26, 26, 1)',
+            fill: false,
+            backgroundColor: 'rgba(255, 51, 51, 0.5)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 12000,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'BwindiElephants.xlsx'
+            }
+        }
+    }
+});
---
+++ GVTC/js/patrol-traffick-cropraid.js
@@ -0,0 +1,140 @@
+//adding the progress charts
+$(function() {
+    $('.chart').easyPieChart({
+        scaleColor:false,
+        lineWidth: 12,
+        lineCap: 'square',
+        trackColor: '#e5e5e5',
+        barColor: '#228B22'
+    });
+});
+
+
+// Trafficking2016 graph
+var ctx = document.getElementsByClassName("bar-chart1");
+
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(255, 165, 0, 1.8)',
+            fill: false,
+            backgroundColor: 'rgba(255, 239, 213, 1)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 400,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'Trafficking2016.xlsx'
+            }
+        }
+    }
+});
+
+
+// Trafficking2017 graph
+var ctx = document.getElementsByClassName("bar-chart2");
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(255, 165, 0, 1.8)',
+            fill: false,
+            backgroundColor: 'rgba(255, 239, 213, 1)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 700,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'Trafficking2017.xlsx'
+            }
+        }
+    }
+});
+
+
+// crop raids Mgahinga graph
+var ctx = document.getElementsByClassName("bar-chart3");
+
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(0, 32, 128, 1.8)',
+            fill: false,
+            backgroundColor: 'rgba(77, 121, 255, 0.7)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 10,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'cropraidsMgahinga.xlsx'
+            }
+        }
+    }
+});
+
+
+// crop raids Bwindi graph
+var ctx = document.getElementsByClassName("bar-chart4");
+var chartGraph = new Chart (ctx, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            borderWidth: 1,
+            borderColor: 'rgba(0, 32, 128, 1.8)',
+            fill: false,
+            backgroundColor: 'rgba(77, 121, 255, 0.7)',
+            opacity: 0.2
+        }
+    ]},
+    plugins: [ChartDataSource],
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 25,
+                    min: 0,
+                }
+            }]
+        },
+        plugins: {
+            datasource: {
+                url: 'cropraidsBwindi.xlsx'
+            }
+        }
+    }
+});
