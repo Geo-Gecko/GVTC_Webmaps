137954d93619b9df8d8aec4e51e87a5f99526ced
Geo Gecko - Uganda <info@geogecko.com>
2019-10-28 16:39:32 +0300

    Update map3.js

--- js/map3.js
+++ js/map3.js
@@ -165,7 +165,7 @@ var baseMaps = {
   "LandCover Classification": landcover
 };
 var layMaps = {
-  "Boundary Conflicts": Conflict
+  "Border Conflicts": Conflict
 };
 
 L.control.layers( baseMaps, layMaps,{
