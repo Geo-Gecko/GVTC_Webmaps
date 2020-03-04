4d128690fa2913d4bc36b03756aac5a05f2fd93a
RonKbS <stephen.bamwite@gmail.com>
2020-03-04 22:21:33 +0300

    more css changes for Ats-Dtn page

--- GVTC/js/arrests_deforestn_/arrests.js
+++ GVTC/js/arrests_deforestn_/arrests.js
@@ -30,6 +30,7 @@ axios.get(url_, { mode: 'no-cors' })
             if (i === 0) {
                 slide_.setAttribute("class", "slide active-slide")
             }
+            slide_.setAttribute("style", "position: relative;")
             let t1 = document.createElement("div")
             t1.setAttribute("class", "table-responsive")
             t1.setAttribute("id", `myDynamicTable${year}`)
@@ -61,13 +62,13 @@ axios.get(url_, { mode: 'no-cors' })
             tr_.appendChild(di_);
             let di1_ = document.createElement("div")
             // console.log(arrests[4][`Arrested(2017)`])
-            di1_.innerHTML += `<tr><th><span><div><h4>${arrests[4][`Arrested(${year})`]} <img src="images/handcuffs3.png" height="60" width="75" align="middle"> </h4></div></span><br><h5>Arrested</h5></th></tr>`
+            di1_.innerHTML += `<tr><th><span><div><h4>${arrests[4][`Arrested(${year})`]} <img src="images/handcuffs3.png" height="60" width="75" align="middle"> </h4></div></span><h5>Arrested</h5></th></tr>`
             tr1_.appendChild(di1_);
             let di2_ = document.createElement("div")
-            di2_.innerHTML += `<tr><th><h4>${arrests[4][`Prosecuted(${year})`]} <img src="images/prosecuted4.png" height="60" width="80" align="middle"> </h4><br><h5>Prosecuted</h5></th></tr>`
+            di2_.innerHTML += `<tr><th><h4>${arrests[4][`Prosecuted(${year})`]} <img src="images/prosecuted4.png" height="60" width="80" align="middle"> </h4><h5>Prosecuted</h5></th></tr>`
             tr2_.appendChild(di2_);
             let di3_ = document.createElement("div")
-            di3_.innerHTML += `<tr><th><h4>${arrests[4][`Sentenced(${year})`]} <img src="images/sentenced.png" height="60" width="80" align="middle"> </h4><br><h5>Sentenced</h5></th></tr>`
+            di3_.innerHTML += `<tr><th><h4>${arrests[4][`Sentenced(${year})`]} <img src="images/sentenced.png" height="60" width="80" align="middle"> </h4><h5>Sentenced</h5></th></tr>`
             tr3_.appendChild(di3_);
 
             tableHead.appendChild(tr_);
@@ -106,15 +107,15 @@ axios.get(url_, { mode: 'no-cors' })
                 td_.appendChild(de_);
                 let de1_ = document.createElement("div")
                 de1_.innerHTML +=
-                    `${a[`Arrested(${year})`] > 50 ? a[`Arrested(${year})`] + '<br><br><img src="images/person1.png" width="11px" height="30px"> = 10' : a[`Arrested(${year})`]}<br/><br/>${images_(parseInt(a[`Arrested(${year})`])).join("")}`
+                    `${a[`Arrested(${year})`] > 50 ? a[`Arrested(${year})`] + '<br><img src="images/person1.png" width="11px" height="30px"> = 10' : a[`Arrested(${year})`]}<br/>${images_(parseInt(a[`Arrested(${year})`])).join("")}`
                 td1_.appendChild(de1_);
                 let de2_ = document.createElement("div")
                 de2_.innerHTML +=
-                    `${a[`Prosecuted(${year})`] > 50 ? a[`Prosecuted(${year})`] + '<br><br><img src="images/person1.png" width="11px" height="30px"> = 10' : a[`Prosecuted(${year})`]}<br/><br/>${images_(parseInt(a[`Prosecuted(${year})`])).join("")}`
+                    `${a[`Prosecuted(${year})`] > 50 ? a[`Prosecuted(${year})`] + '<br><img src="images/person1.png" width="11px" height="30px"> = 10' : a[`Prosecuted(${year})`]}<br/>${images_(parseInt(a[`Prosecuted(${year})`])).join("")}`
                 td2_.appendChild(de2_);
                 let de3_ = document.createElement("div")
                 de3_.innerHTML +=
-                    `${a[`Sentenced(${year})`] > 50 ? a[`Sentenced(${year})`] + '<br><br><img src="images/person1.png" width="11px" height="30px"> = 10' : a[`Sentenced(${year})`]}<br/><br/>${images_(parseInt(a[`Sentenced(${year})`])).join("")}`
+                    `${a[`Sentenced(${year})`] > 50 ? a[`Sentenced(${year})`] + '<br><img src="images/person1.png" width="11px" height="30px"> = 10' : a[`Sentenced(${year})`]}<br/>${images_(parseInt(a[`Sentenced(${year})`])).join("")}`
                 td3_.appendChild(de3_);
 
                 row_.appendChild(td_);
